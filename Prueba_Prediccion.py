a0c94fcf84cfbfb1e2b8358cc621ef65c8ec6aa4
FosterTooth1 <albertoflores91396@gmail.com>
2025-10-25 21:32:30 -0600

    sdffdsfssfdsdf

--- Prueba_Prediccion.py
+++ Prueba_Prediccion.py
@@ -9,7 +9,7 @@ encoders_path_local = "label_encoders.pkl"
 Modelo_RandomForest = joblib.load(model_path_local)
 label_encoders = joblib.load(encoders_path_local)
 
-# Cargar nuevos datos para predecir
+# Ejemplo: cargar nuevos datos para predecir
 df_new = pd.read_csv("Prueba_Prediccion.csv")
 
 # Aplicar codificación usando los encoders cargados
@@ -17,12 +17,12 @@ for col, le in label_encoders.items():
     if col in df_new.columns:
         df_new[col] = le.transform(df_new[col])
 
-X_new = df_new.drop("Condition", axis=1, errors='ignore')  
+X_new = df_new.drop("Condition", axis=1, errors='ignore')  # si 'Condition' no existe, ignóralo
 
 # Hacer predicciones
 y_pred_new = Modelo_RandomForest.predict(X_new)
 
-# Convertir las predicciones de vuelta a clases originales:
+# Si quieres convertir las predicciones de vuelta a clases originales:
 clases = label_encoders["Condition"].classes_
 y_pred_labels = clases[y_pred_new]
 print(y_pred_labels)
